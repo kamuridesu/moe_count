ce317e21e1b4e90a840ae0b7bd2f56be435fa451
Kamuri Amorim <myk.gata14@gmail.com>
2023-03-28 08:55:23 -0300

    update db.py

--- src/db.py
+++ src/db.py
@@ -1,59 +1,38 @@
 import sqlite3
-
+import threading
 
 class DB:
     def __init__(self, dbname):
-        self.locked = False
         self.conn = sqlite3.connect(dbname)
-        
         self.conn.execute("""CREATE TABLE IF NOT EXISTS "users" (
-            "username"	TEXT NOT NULL UNIQUE,
-            "count"	INTEGER NOT NULL,
+            "username"  TEXT NOT NULL UNIQUE,
+            "count"     INTEGER NOT NULL,
             PRIMARY KEY("username")
         );""")
+        self.lock = threading.Lock()
 
-    def lock(self):
-        self.locked = True
-
-    def unlock(self):
-        self.locked = False
-
-    def insert(self, query, *params):
-        while self.locked:
-            pass
-        self.lock()
+    def execute(self, query, *params):
+        self.lock.acquire()
         cursor = self.conn.cursor()
         cursor.execute(query, params)
         cursor.close()
         self.conn.commit()
-        self.unlock()
+        self.lock.release()
     
     def addUser(self, username):
         query = """INSERT INTO "users" (username, count) VALUES (?, ?)"""
-        self.insert(query, username, 0)
+        self.execute(query, username, 0)
 
     def addCount(self, username, count: int):
         query = """UPDATE users SET count=? WHERE username=?"""
-        self.insert(query, count, username)
+        self.execute(query, count, username)
         
     def search(self, username: str) -> int | None:
-        while self.locked:
-            pass
-        self.lock()
         cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE username=\"%s\"" % username)
+        cursor.execute("SELECT * FROM users WHERE username=?", (username,))
         records = cursor.fetchall()
         cursor.close()
-        self.unlock()
         return records
 
     def close(self):
         self.conn.close()
-
-
-if __name__ == '__main__':
-    db = DB()
-    db.addUser("kamuridesu")
-    for i in range(10):
-        actual_cuont = db.search("kamuridesu")
-        db.addCount("kamuridesu", actual_cuont + 1)
